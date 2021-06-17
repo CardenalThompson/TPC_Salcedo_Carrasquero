5afa034b3b06dc5678ce61c80e50f9189a849858
Tomás Carrasquero <cardenalthompson@hotmail.com>
2021-06-17 20:59:57 -0300

    Agregando clases

---
+++ Dominio/Direccion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Direccion
+    {
+        public string Calle { get; set; }
+        public int Piso { get; set; }
+        public char Departamento { get; set; }
+        public string Ciudad { get; set; }
+        public int CodPostal { get; set; }
+    }
+}
